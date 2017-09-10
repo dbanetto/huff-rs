d698591399d2d27fad5a3643de5ea24dd4212522
David Barnett <david@barnett.net.nz>
2017-09-10 17:21:00 +1200

    Implement HuffReader

--- src/lib.rs
+++ src/lib.rs
@@ -9,26 +9,12 @@ use std::collections::HashMap;
 use std::hash::Hash;
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum TreeNode<V: Eq> {
+pub enum TreeNode<V: Eq + Copy> {
     Leaf(V),
     Node(Box<TreeNode<V>>, Box<TreeNode<V>>),
 }
 
-impl <V: Eq> TreeNode<V> {
-
-    fn value<'a>(self) -> Option<V> {
-        match self {
-            TreeNode::Leaf(v) => Some(v),
-            TreeNode::Node(_,_) => None,
-        }
-    }
-
-    fn is_leaf(&self) -> bool {
-        match self {
-            &TreeNode::Leaf(_) => true,
-            &TreeNode::Node(_,_) => false,
-        }
-    }
+impl <V: Eq + Copy> TreeNode<V> {
 
     pub fn new_leaf(value: V) -> Self {
         TreeNode::Leaf(value)
@@ -40,7 +26,7 @@ impl <V: Eq> TreeNode<V> {
 
 }
 
-impl <V: Eq + Hash> TreeNode<V> {
+impl <V: Eq + Copy + Hash> TreeNode<V> {
 
     pub fn encoding(self) -> HashMap<V, Vec<bool>> {
         let trail: Vec<bool> = vec![];
@@ -57,10 +43,12 @@ impl <V: Eq + Hash> TreeNode<V> {
                 map.insert(v, trail.clone());
             },
             TreeNode::Node(l, r) => {
+
                 //handle left
                 let mut left = trail.clone();
                 left.push(false);
                 l.build_map(left, map);
+
                 //handle right
                 let mut right = trail.clone();
                 right.push(true);
@@ -70,11 +58,11 @@ impl <V: Eq + Hash> TreeNode<V> {
     }
 }
 
-pub struct TreeBuilder<V: Eq, W: PartialOrd + Add<Output=W>> {
+pub struct TreeBuilder<V: Eq + Copy, W: PartialOrd + Add<Output=W>> {
     nodes: Vec<(V, W)>
 }
 
-impl <V: Eq, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
+impl <V: Eq + Copy, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
     pub fn new() -> Self {
         TreeBuilder {
             nodes: vec![],
@@ -131,7 +119,7 @@ impl <V: Eq, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
     }
 }
 
-impl <V: Eq + Hash, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
+impl <V: Eq + Copy + Hash, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
 
     pub fn add_table<I>(mut self, table: I) -> Self
     where I: IntoIterator<Item=(V, W)> {
@@ -143,12 +131,12 @@ impl <V: Eq + Hash, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
     }
 }
 
-pub struct HuffWriter<V: Eq + Hash, W: Write> {
+pub struct HuffWriter<V: Eq + Copy + Hash, W: Write> {
     encoding: HashMap<V, Vec<bool>>,
     writer: BitWriter<W, NoPadding>,
 }
 
-impl <V: Eq + Hash, W: Write> HuffWriter<V, W> {
+impl <V: Eq + Copy + Hash, W: Write> HuffWriter<V, W> {
 
     pub fn new(tree : TreeNode<V>, writer: W) -> Self {
         HuffWriter {
@@ -168,6 +156,45 @@ impl <V: Eq + Hash, W: Write> HuffWriter<V, W> {
         }
 
         Ok(())
+    }
+}
+
+pub struct HuffReader<V: Eq + Copy, R: Read> {
+    tree: Box<TreeNode<V>>,
+    reader: BitReader<R, NoPadding>,
+}
+
+impl <V: Eq + Copy, R: Read> HuffReader<V, R> {
+
+    pub fn new(tree: TreeNode<V>, reader: R) -> Self {
+        HuffReader {
+            tree: Box::new(tree),
+            reader: BitReader::new(reader),
+        }
+    }
+
+    pub fn read(&mut self) -> std::io::Result<V> {
+
+        let mut cursor = &self.tree;
+
+        loop {
+            match **cursor {
+                TreeNode::Leaf(ref value) => return Ok(*value),
+                TreeNode::Node(ref l, ref r) => {
+                    let bit = self.reader.read_bit()?;
+                    match bit {
+                        Some(b) => {
+                            if b {
+                                cursor = r;
+                            } else {
+                                cursor = l;
+                            };
+                        },
+                        None => return Err(Error::from(ErrorKind::UnexpectedEof)),
+                    }
+                },
+            };
+        }
     }
 }
 
@@ -295,5 +322,30 @@ mod tests {
                 }
             }
         }
+    }
+
+    use std::io::Cursor;
+
+    #[test]
+    fn decode() {
+
+        let tree = TreeBuilder::<char, u32>::new()
+            .add('a', 1)
+            .add('b', 1)
+            .add('c', 1)
+            .add('d', 1)
+            .build()
+            .unwrap();
+
+        let input = vec![0b_00011011, 0b_00000000];
+
+        let mut reader = HuffReader::new(tree, Cursor::new(input));
+        let mut output = vec![];
+
+        for _ in 0 .. 5 {
+           output.push(reader.read().unwrap());
+        }
+
+        assert_eq!(vec!['a', 'b', 'c', 'd', 'a'], output);
     }
 }
