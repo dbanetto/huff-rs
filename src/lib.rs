f655d4ecd8d68f828920ade015c3f71cb9d60e62
David Barnett <david@barnett.net.nz>
2017-09-10 16:32:59 +1200

    Implement huffman writer

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
+extern crate bitstream;
+
+use bitstream::{BitWriter, BitReader, NoPadding};
+
+use std::io::prelude::*;
+use std::io::{Error, ErrorKind};
 use std::ops::Add;
 use std::collections::HashMap;
 use std::hash::Hash;
 
 #[derive(Debug, PartialEq, Eq)]
-enum TreeNode<V: Eq> {
+pub enum TreeNode<V: Eq> {
     Leaf(V),
     Node(Box<TreeNode<V>>, Box<TreeNode<V>>),
 }
@@ -46,7 +52,6 @@ impl <V: Eq + Hash> TreeNode<V> {
     }
 
     fn build_map(self, trail: Vec<bool>, map :&mut HashMap<V, Vec<bool>>) {
-        
         match self {
             TreeNode::Leaf(v) => {
                 map.insert(v, trail.clone());
@@ -65,12 +70,11 @@ impl <V: Eq + Hash> TreeNode<V> {
     }
 }
 
-struct TreeBuilder<V: Eq, W: PartialOrd> { 
+pub struct TreeBuilder<V: Eq, W: PartialOrd + Add<Output=W>> { 
     nodes: Vec<(V, W)>
 }
 
 impl <V: Eq, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
-    
     pub fn new() -> Self {
         TreeBuilder {
             nodes: vec![],
@@ -128,9 +132,37 @@ impl <V: Eq, W: PartialOrd + Add<Output=W>> TreeBuilder<V, W> {
     }
 }
 
+struct HuffWriter<V: Eq + Hash, W: Write> {
+    encoding: HashMap<V, Vec<bool>>,
+    writer: BitWriter<W, NoPadding>,
+}
+
+impl <V: Eq + Hash, W: Write> HuffWriter<V, W> {
+    
+    pub fn new(tree : TreeNode<V>, writer: W) -> Self {
+        HuffWriter {
+            encoding: tree.encoding(),
+            writer: BitWriter::new(writer),
+        }
+    }
+
+    pub fn write(&mut self, value: &V) -> std::io::Result<()> {
+        let bits: &Vec<bool> = match self.encoding.get(value) {
+           Some(bits) => bits, 
+           None => { return Err(Error::from(ErrorKind::InvalidInput)); },
+        };
+        for bit in bits {
+            self.writer.write_bit(*bit)?;
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
     
     #[test]
     fn build_simple_tree() {
@@ -189,5 +221,49 @@ mod tests {
         expected.insert('d', vec![true, true]);
 
         assert_eq!(expected, tree.encoding());
+    }
+    
+    #[test]
+    fn encode() {
+        let tree = TreeBuilder::<char, u32>::new()
+            .add('a', 1)
+            .add('b', 1)
+            .add('c', 1)
+            .add('d', 1)
+            .build()
+            .unwrap();
+
+        let mut output: Vec<u8> = vec![];
+        {
+            let mut writer = HuffWriter::new(tree, &mut output);
+
+            for value in vec!['a', 'b', 'c', 'd', 'a'] {
+                writer.write(&value).unwrap();
+            }
+        }
+        
+        let expected = vec![0b_00011011, 0b_00000000];
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn encode_value_error() {
+        let tree = TreeBuilder::<char, u32>::new()
+            .add('a', 1)
+            .build()
+            .unwrap();
+
+        let mut output: Vec<u8> = vec![];
+        {
+            let mut writer = HuffWriter::new(tree, &mut output);
+
+            for value in vec!['b'] {
+                match writer.write(&value) {
+                    Ok(_) => panic!(),
+                    Err(_) => (),
+                }
+            }
+        }
     }
 }
